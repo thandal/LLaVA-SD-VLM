c5d0340130cea7d15249a51eb887e2063a6d1f61
thandal <than@timbrel.org>
2026-01-30 17:55:45 -0500

    More quickstart cleanup, move to 8bit

--- quickstart.py
+++ quickstart.py
@@ -1,11 +1,9 @@
 from llava.model.builder import load_pretrained_model
 from llava.mm_utils import get_model_name_from_path
-from llava.eval.run_llava import eval_model
 from llava.mm_utils import tokenizer_image_token, process_images
 from llava.constants import IMAGE_TOKEN_INDEX
-import copy
+
 from PIL import Image
-import os
 import torch
 
 model_path = "cpystan/SD-VLM-7B"
@@ -14,18 +12,17 @@ tokenizer, model, image_processor, context_len = load_pretrained_model(
     model_path=model_path,
     model_name=get_model_name_from_path(model_path),
     model_base=None,
-    load_4bit=True,
+    load_8bit=True,
+    load_4bit=False,
     mm_vision_tower="openai/clip-vit-large-patch14-336"
 )
 model.gt_depth = False
 
-prompt = "how big is the table?"
-image_folder = "example_data"
-image_file = "table_256.jpg"
+prompt = "What is the width of this table?"
+image_file = "example_data/table.jpg"
 
 input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
-image = Image.open(os.path.join(image_folder, image_file)).convert('RGB')
-ori_img = copy.deepcopy(image)
+image = Image.open(image_file).convert('RGB')
 image_tensor = process_images([image], image_processor, model.config)[0]
 
 temperature = 0.2
@@ -38,7 +35,7 @@ with torch.inference_mode():
         temperature=temperature,
         top_p=None,
         num_beams=1,
-        ori_imgs = [ori_img],
+        ori_imgs = [image],
         max_new_tokens=1024,
         use_cache=True,)
 
